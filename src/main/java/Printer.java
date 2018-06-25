7d868216e84ef2b8b6963093c73cafb48f72d6a5
Gary Muir <garymuir17@gmail.com>
2018-06-25 15:58:02 +0100

    adds the Printer class, and test a getter

---
+++ src/main/java/Printer.java
@@ -0,0 +1,13 @@
+public class Printer {
+
+
+    private int paperLeft;
+
+    public Printer(int paperLeft) {
+        this.paperLeft = paperLeft;
+    }
+
+    public int getPaperLeft() {
+        return this.paperLeft;
+    }
+}
